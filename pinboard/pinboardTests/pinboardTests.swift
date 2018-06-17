78ed8e282231cb6488ea5058ebb749295f08d101
cembaykara <babayk@ttu.ee>
2018-06-17 16:04:04 +0300

    fetcher testExample

--- pinboard/pinboardTests/pinboardTests.swift
+++ pinboard/pinboardTests/pinboardTests.swift
@@ -7,29 +7,19 @@
 //
 
 import XCTest
+import GenericFetcher
 
 class pinboardTests: XCTestCase {
     
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+        let fetcher = Fetcher()
+        var error = false
+        fetcher.fetch(with: .json, urlStr: "http://pastebin.com/raw/wgkJgazE") { (data: [Object], _, err) in
+            if err != nil {
+                error = true
+            }
         }
+        XCTAssert(!error)
     }
     
 }
