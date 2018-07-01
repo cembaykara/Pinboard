5c592c130efc4bdd0389fb9f93f4651bc4ec6641
cembaykara <babayk@ttu.ee>
2018-07-02 01:39:07 +0300

    update Screenshot

---
+++ pinboard/pinboard/Helper/CardAnimationHandler.swift
@@ -0,0 +1,9 @@
+//
+//  CardAnimationHandler.swift
+//  pinboard
+//
+//  Created by Baris Cem Baykara on /17/18.
+//  Copyright © 2018 Baris Cem Baykara. All rights reserved.
+//
+
+import Foundation
